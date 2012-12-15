59b986cd61d1d311563834a122dd7ec93f29a834
Jesse Donat <j.donat@capstonedigital.com>
2012-12-14 22:24:02 -0600

    Some code cleanup

--- mjpeg.php
+++ mjpeg.php
@@ -1,29 +1,25 @@
 <?php
-# Used to separate multipart
+
+// Used to separate multipart
 $boundary = "spiderman";
 
-# We start with the standard headers. PHP allows us this much
+// We start with the standard headers. PHP allows us this much
 header("Cache-Control: no-cache");
 header("Cache-Control: private");
 header("Pragma: no-cache");
 header("Content-type: multipart/x-mixed-replace; boundary=$boundary");
 
-# From here out, we no longer expect to be able to use the header() function
-echo "--$boundary\n\n";
-
-# Set this so PHP doesn't timeout during a long stream
+// Set this so PHP doesn't timeout during a long stream
 set_time_limit(0);
-
-# The loop, producing one jpeg frame per iteration
 
 $rand = rand(1, 10000);
 
 $i = rand(1, 10000);
 while(true) {
 	$i++;
-	# Per-image header, note the two new-lines
-	echo "Content-type: image/jpeg\n\n";
-
+	
+	// Per-image header, note the two new-lines
+	
 	$im = imagecreatetruecolor(400, 100);
 	$fill_color = imagecolorallocate($im, (128 * sin($i / 30)) + 128, (128 * sin($i / 25)) + 128, (128 * sin($i / 20)) + 128);
 	imagefill($im,1,1,$fill_color);
@@ -34,8 +30,8 @@ while(true) {
 	imagedestroy($im);
 	echo ob_get_clean(); 
 	
-
+	usleep(10000);
 
 	echo "--$boundary\n";
-	usleep(10000);
+	echo "Content-type: image/jpeg\n\n";
 }
